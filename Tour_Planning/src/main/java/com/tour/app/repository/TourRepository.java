0fa9cc281688c7491061187c2ef525b8b8f5ced7
Jagadeesh23520 <82135265+Jagadeesh23520@users.noreply.github.com>
2022-10-08 18:28:33 +0530

    Delete Tour_Planning directory

--- Tour_Planning/src/main/java/com/tour/app/repository/TourRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tour.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tour.app.model.TourDetails;
-
-public interface TourRepository extends JpaRepository<TourDetails, Integer> {
-
-}
